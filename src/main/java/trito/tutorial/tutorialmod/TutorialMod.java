ff647be3fec1eb9b8369dc7c3a2dfdc8366c902a
Trito <tritonusgm@gmail.com>
2026-03-22 19:51:00 +0100

    Comentario añadido

--- src/main/java/trito/tutorial/tutorialmod/TutorialMod.java
+++ src/main/java/trito/tutorial/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//Comentario importante
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
