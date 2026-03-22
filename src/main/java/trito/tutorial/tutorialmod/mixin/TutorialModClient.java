7002a0f22803d2e3b56a93ac485ddf2a3e4d07bb
Trito <tritonusgm@gmail.com>
2026-03-22 19:47:54 +0100

    Initial commit

---
+++ src/main/java/trito/tutorial/tutorialmod/mixin/TutorialModClient.java
@@ -0,0 +1,10 @@
+package trito.tutorial.tutorialmod.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
